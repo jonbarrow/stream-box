2e992f3a49a851a530abdb4f5aecd7be68a813c2
RedDucks <jonbarrow1998@gmail.com>
2019-07-18 01:49:23 -0400

    Added keybind to exit player on Windows

--- app/assets/js/player.js
+++ app/assets/js/player.js
@@ -23,6 +23,10 @@ const player = new Plyr(video, {
 	]
 });
 
+player.on('canplay', () => {
+	player.play();
+});
+
 const playerWrapper = document.getElementById('player-wrapper');
 
 function showPlayer() {
@@ -32,6 +36,10 @@ function showPlayer() {
 }
 
 function hidePlayer() {
+	if (player.playing) {
+		player.pause();
+	}
+
 	if (playerOpen()) {
 		playerWrapper.classList.add('hide');
 	}
@@ -77,90 +85,111 @@ function startStream(stream) {
 	}
 }
 
+function piKeyHandle(event) {
+	if (!omxplayer.isPlaying()) {
+		return;
+	}
+
+	event.preventDefault();
+	
+	const {key} = event;
+	
+	switch (key) {
+		case '1':
+			omxplayer.decreaseSpeed();
+			break;
+		case '2':
+			omxplayer.increaseSpeed();
+			break;
+		case '<':
+			omxplayer.rewind();
+			break;
+		case '>':
+			omxplayer.fastForward();
+			break;
+		case 'z':
+			omxplayer.showInfo();
+			break;
+		case 'j':
+			omxplayer.previousAudioStream();
+			break;
+		case 'k':
+			omxplayer.nextAudioStream();
+			break;
+		case 'i':
+			omxplayer.previousChapter();
+			break;
+		case 'o':
+			omxplayer.nextChapter();
+			break;
+		case 'n':
+			omxplayer.previousSubtitleStream();
+			break;
+		case 'm':
+			omxplayer.nextSubtitleStream();
+			break;
+		case 's':
+			omxplayer.toggleSubtitles();
+			break;
+		case 'w':
+			omxplayer.showSubtitles();
+			break;
+		case 'x':
+			omxplayer.hideSubtitles();
+			break;
+		case 'd':
+			omxplayer.decreaseSubtitleDelay();
+			break;
+		case 'f':
+			omxplayer.increaseSubtitleDelay();
+			break;
+		case 'q':
+			omxplayer.quit();
+			break;
+		case 'p': case ' ':
+			omxplayer.togglePlay();
+			break;
+		case '-':
+			omxplayer.decreaseVolume();
+			break;
+		case '+': case '=':
+			omxplayer.increaseVolume();
+			break;
+		case 'ArrowLeft':
+			omxplayer.seekBack30();
+			break;
+		case 'ArrowRight':
+			omxplayer.seekForward30();
+			break;
+		case 'ArrowDown':
+			omxplayer.seekBack600();
+			break;
+		case 'ArrowUp':
+			omxplayer.seekForward600();
+			break;
+	}
+}
+
+function keyHandle(event) {
+
+	if (player.playing) {
+		event.preventDefault();
+	}
+	
+	const {key} = event;
+
+	if (key === 'Escape' && player.playing) {
+		hidePlayer();
+	}
+}
+
 // Get around eslint no-unused-vars, and make 100% sure the variables are global
 !function() {
 	document.addEventListener('keydown', event => {
-		if (!omxplayer.isPlaying()) {
-			return;
-		}
-
-		event.preventDefault();
-		
-		const {key} = event;
-		
-		switch (key) {
-			case '1':
-				omxplayer.decreaseSpeed();
-				break;
-			case '2':
-				omxplayer.increaseSpeed();
-				break;
-			case '<':
-				omxplayer.rewind();
-				break;
-			case '>':
-				omxplayer.fastForward();
-				break;
-			case 'z':
-				omxplayer.showInfo();
-				break;
-			case 'j':
-				omxplayer.previousAudioStream();
-				break;
-			case 'k':
-				omxplayer.nextAudioStream();
-				break;
-			case 'i':
-				omxplayer.previousChapter();
-				break;
-			case 'o':
-				omxplayer.nextChapter();
-				break;
-			case 'n':
-				omxplayer.previousSubtitleStream();
-				break;
-			case 'm':
-				omxplayer.nextSubtitleStream();
-				break;
-			case 's':
-				omxplayer.toggleSubtitles();
-				break;
-			case 'w':
-				omxplayer.showSubtitles();
-				break;
-			case 'x':
-				omxplayer.hideSubtitles();
-				break;
-			case 'd':
-				omxplayer.decreaseSubtitleDelay();
-				break;
-			case 'f':
-				omxplayer.increaseSubtitleDelay();
-				break;
-			case 'q':
-				omxplayer.quit();
-				break;
-			case 'p': case ' ':
-				omxplayer.togglePlay();
-				break;
-			case '-':
-				omxplayer.decreaseVolume();
-				break;
-			case '+': case '=':
-				omxplayer.increaseVolume();
-				break;
-			case 'ArrowLeft':
-				omxplayer.seekBack30();
-				break;
-			case 'ArrowRight':
-				omxplayer.seekForward30();
-				break;
-			case 'ArrowDown':
-				omxplayer.seekBack600();
-				break;
-			case 'ArrowUp':
-				omxplayer.seekForward600();
-				break;
+		if (isPi()) {
+			piKeyHandle(event);
+		} else {
+			keyHandle(event);
 		}
 	});
 
